e711b0bb0e4126226b35f3285819b36d6d6a0496
Djery-Tom <dietchidjery@gmail.com>
2021-09-18 14:06:23 +0100

    Fix migration and update card type

--- database/gift_cards_api.sql
+++ database/gift_cards_api.sql
@@ -1,89 +1,48 @@
--- MySQL dump 10.13  Distrib 8.0.26, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: gift-cards-api
--- ------------------------------------------------------
--- Server version	8.0.26-0ubuntu0.20.04.2
+-- Hôte : localhost:3306
+-- Généré le : sam. 18 sep. 2021 à 13:57
+-- Version du serveur :  8.0.26-0ubuntu0.20.04.2
+-- Version de PHP : 8.0.10
+
+SET
+SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET
+AUTOCOMMIT = 0;
+START TRANSACTION;
+SET
+time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `cards`
+-- Base de données : `gift-cards-api`
 --
 
-DROP TABLE IF EXISTS `cards`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cards`
-(
-    `id`      int          NOT NULL AUTO_INCREMENT,
-    `name`    varchar(255) NOT NULL,
-    `value`   int DEFAULT '0',
-    `type_id` int          NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
--- Dumping data for table `cards`
+-- Déchargement des données de la table `cards`
 --
 
-LOCK
-TABLES `cards` WRITE;
-/*!40000 ALTER TABLE `cards` DISABLE KEYS */;
 INSERT INTO `cards` (`id`, `name`, `value`, `type_id`)
 VALUES (1, 'Card A', 100, 1),
        (2, 'Card B', 100, 3);
-/*!40000 ALTER TABLE `cards` ENABLE KEYS */;
-UNLOCK
-TABLES;
 
 --
--- Table structure for table `cards_types`
+-- Déchargement des données de la table `cards_types`
 --
 
-DROP TABLE IF EXISTS `cards_types`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cards_types`
-(
-    `id`              int          NOT NULL AUTO_INCREMENT,
-    `name`            varchar(255) NOT NULL,
-    `max_avail_value` int DEFAULT '0',
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cards_types`
---
-
-LOCK
-TABLES `cards_types` WRITE;
-/*!40000 ALTER TABLE `cards_types` DISABLE KEYS */;
 INSERT INTO `cards_types` (`id`, `name`, `max_avail_value`)
 VALUES (1, 'Type 1', 100),
        (2, 'Type 2', 50),
        (3, 'Type 3', 30);
-/*!40000 ALTER TABLE `cards_types` ENABLE KEYS */;
-UNLOCK
-TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+COMMIT;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-09-17 11:00:19
